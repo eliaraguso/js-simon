5f8b5f0317dff817d67fe2dae6fbd62f2a0c8790
eliaraguso <elia.raguso@gmail.com>
2021-09-06 14:17:15 +0200

    creato intervallo di tempo dopo il quale l'utente inserisce i 5 numeri che deve ricordare per vincere

--- main.js
+++ main.js
@@ -4,16 +4,43 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // Definisco un array contenente 5 numeri casuali con range a 0 a 100
-let arrayNumeri = [];
+let arrayNumeriCasuali = [];
 
 
 /* riempio l'array con in numeri con un ciclo che viene eseguito fintanto che 
 il numero degli elementi nell'array non è uguale a 5 */
 
-while(arrayNumeri.length < 5){
+while(arrayNumeriCasuali.length < 5){
     var r = Math.floor(Math.random() * 100) + 1;
-    if(arrayNumeri.indexOf(r) === -1) arrayNumeri.push(r);
+    if(arrayNumeriCasuali.indexOf(r) === -1) arrayNumeriCasuali.push(r);
 }
 
+
 // eseguo un alert che espone il mio array appena creato
-alert("I numeri da ricordare sono:" + " " + arrayNumeri);
+
+alert("I numeri da ricordare sono:" + " " + arrayNumeriCasuali);
+
+
+// definisco un array vuoto che andrà riempito con i numeri che inserirà l'utente
+
+let arrayNumeriUtente = [];
+
+
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i 5 numeri che ha visto precedentemente, tramite 5 prompt().
+
+setTimeout(
+    function() {
+       for (let index = 0; index < 5; index++) {
+           arrayNumeriUtente.push(parseInt(prompt("Inserisci un numero!!")));
+       }
+       if (arrayNumeriUtente.length = 5) {
+        alert("I numeri inseriti sono:" + " " + arrayNumeriUtente);
+       }
+    }, 
+    3000
+);
+
+
+
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
